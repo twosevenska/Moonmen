923bc845a6ed481ff4331b52c45d6248ac054538
Eduardo Pereira <eduardop@student.dei.uc.pt>
2016-06-18 21:07:55 +0100

    Finished Script 1.0

--- dodgeFlowey/script.cpp
+++ dodgeFlowey/script.cpp
@@ -10,7 +10,6 @@ boolean gotBall = false;
 //Cientistas, Vidro, Bola, Targets pequenos, Target grande, Fog, end level 
 void scripting(GLint *actions) {
 	
-	
 	frames++;
 
 	if(frames == 1)
@@ -27,4 +26,11 @@ void scripting(GLint *actions) {
 	if (actions[3] == 1 && gotBall) {
 		actions[3] = 2;
 	}
+
+	if (actions[4] == 2 && actions[5] == 0) {
+		actions[5] = 1;
+	}
+
+	if (actions[5] == 2)
+		actions[6] = 2;
 }
