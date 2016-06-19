147357743d24228d1c493095a30dd99c0b42f359
Flavio Penas <Flavio Penas>
2016-06-19 17:35:33 +0100

    text

--- dodgeFlowey/text.cpp
+++ dodgeFlowey/text.cpp
@@ -5,18 +5,55 @@ void changetimer(int typetimer, int time, int* timer)
 	timer[typetimer] = time;
 }
 
+void crosshair(int wScreen, int hScreen)
+{
+	glEnable(GL_COLOR_MATERIAL);
+	
+	glColor3f(1, 0, 0);
+
+	//GLfloat mat_cross[] = {1.0,0.0,0.0,1.0};
+	//glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_cross);
+
+	glBegin(GL_POLYGON);
+	glVertex3f(wScreen / 2 - 4, hScreen / 2 - 4, -0.5);
+	glVertex3f(wScreen / 2 + 4, hScreen / 2 - 4, -0.5);
+	glVertex3f(wScreen / 2 + 4, hScreen / 2 + 4, -0.5);
+	glVertex3f(wScreen / 2 - 4, hScreen / 2 + 4, -0.5);
+	glEnd();
+	glColor3f(0.6,0.6,0.6);
+	glDisable(GL_COLOR_MATERIAL);
+}
+
 void load_text(int wScreen, int hScreen, char ** text, int * timer)
 {
 	int narray;
+	int flag = 0;
 	for (narray = 0; narray < 7; narray++)
 	{
 		if (timer[narray]>0)
 		{
-			glTranslatef(wScreen / 2 - 200, hScreen / 2 + 50, 0);
-			glScalef(0.3, 0.3, 3);
-			glColor3f(1, 1, 1);
+			flag = 1;
+
+			glEnable(GL_COLOR_MATERIAL);
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+			glEnable(GL_BLEND);
+			glColor4f(HGLASS);
+			glPushMatrix();
+			glBegin(GL_POLYGON);
+			glVertex3f(0, 0, -0.1);
+			glVertex3f(wScreen, 0, -0.1);
+			glVertex3f(wScreen, hScreen, -0.1);
+			glVertex3f(0, hScreen, -0.1);
+			glEnd();
+			glPopMatrix();
+			glDisable(GL_BLEND);
+			glDisable(GL_COLOR_MATERIAL);
+			glTranslatef(wScreen / 2 - 100, hScreen / 2 - 50, 0);
+			glScalef(0.5, 0.5, 0.001);
+			glColor3f(0,0, 0);
 			int x = 0;
 			int theNcount = 0;
+			glEnable(GL_COLOR_MATERIAL);
 			for (int i = 0; i < strlen(text[narray]); i++)
 			{
 				x += glutStrokeWidth(GLUT_STROKE_ROMAN, text[narray][i]);
@@ -39,8 +76,14 @@ void load_text(int wScreen, int hScreen, char ** text, int * timer)
 					x = 0;
 				}
 			}
+			glColor3f(0.6, 0.6, 0.6);
+			glDisable(GL_COLOR_MATERIAL);
 			glEnd();
 			timer[narray]--;
 		}
+	}
+	if (flag == 0)
+	{
+		crosshair(wScreen, hScreen);
 	}
 }
--- dodgeFlowey/text.h
+++ dodgeFlowey/text.h
@@ -1,3 +1,4 @@
 #pragma once
 void changetimer(int typetimer, int time, int* timer);
 void load_text(int wScreen, int hScreen, char ** text, int * timer);
+void crosshair(int wScreen, int hScreen);
